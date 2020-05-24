825274f9c802a1f0eaa4728070b837430ccd153e
Jiacheng Qian <th3chinese@gmx.de>
2020-05-24 13:43:30 +0200

    removed excess gradle imports

--- src/main/java/de/cheng/templateservice/TemplateServiceApplication.java
+++ src/main/java/de/cheng/templateservice/TemplateServiceApplication.java
@@ -2,14 +2,10 @@ package de.cheng.templateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class TemplateServiceApplication {
 
   public static void main(final String[] args) {
